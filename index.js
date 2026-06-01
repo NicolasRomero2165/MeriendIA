1759aa2706d4dac710055a91808dd8749e631b6c
NicolasRomero2165 <nicolas.romero.2165@gmail.com>
2026-06-01 00:08:54 -0300

    Comienzo de registro.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function login(inputEmail, inputPass) {
     inputPass = document.getElementById("password").value
 
     console.log(`Correo: ${inputEmail}`)
-    console.log(`Contraseña: ${inputPass}`)
+    console.log(`Contraseña: ********`)
 
     const loginOk = users.find(
         (item) => item.email === inputEmail && item.password === inputPass
@@ -30,5 +30,26 @@ function login(inputEmail, inputPass) {
     } else {
         alert("Datos incorrectos.")
         return false
+    }
+}
+
+function newUser(inputName, inputEmail, inputNumber, inputPass, inputPass2) {
+
+    inputName = document.getElementById("name").value
+    inputEmail = document.getElementById("email").value
+    inputNumber = document.getElementById("number").value
+    inputPass = document.getElementById("password").value
+    inputPass2 = document.getElementById("password2").value
+
+    console.log(`Nombre completo: ${inputName}`)
+    console.log(`Correo: ${inputEmail}`)
+    console.log(`Número: ${inputNumber}`)
+    console.log(`Contraseña1: ${inputPass}`)
+    console.log(`Contraseña2: ${inputPass2}`)
+
+    if (inputPass === inputPass2) {
+        alert("¡Cuenta creada!")
+    } else {
+        alert("No coinciden las contraseñas.")
     }
 }
